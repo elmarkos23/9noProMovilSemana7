c9a48a5b4441ed13ec180ac93dc3f9cc769f35b2
Markos Ayala <elmarkos23@gmail.com>
2021-11-21 08:25:13 -0500

    carga inicial

---
+++ App1/App1.Android/SqlCliente.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using App1.Droid;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[assembly: Xamarin.Forms.Dependency(typeof(SqlCliente))]
+namespace App1.Droid
+{
+  class SqlCliente : DataBase
+  {
+    public SQLiteAsyncConnection GetConnection()
+    {
+      var documentPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+      var path = System.IO.Path.Combine(documentPath, "uisrael.db3");
+      return new SQLiteAsyncConnection(path);
+    }
+  }
+}
