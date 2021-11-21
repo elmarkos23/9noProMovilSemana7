8886496d83460984bdc5f7c9da4c3b6bc233a46e
Markos Ayala <elmarkos23@gmail.com>
2021-11-21 08:29:37 -0500

    carga inicial 2

---
+++ App1/App1/Models/Estudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace App1.Models
+{
+  public class Estudiante
+  {
+    [PrimaryKey,AutoIncrement]
+    public int Id { get; set; }
+
+    [MaxLength (50)]
+    public string Nombre { get; set; }
+
+    [MaxLength(25)]
+    public string Usuario { get; set; }
+
+    [MaxLength(25)]
+    public string Contrasenia { get; set; }
+
+  }
+}
